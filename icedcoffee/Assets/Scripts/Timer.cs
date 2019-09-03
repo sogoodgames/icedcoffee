43fcd79ae7ced2711b3351a968946803028600b5
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2019-09-02 17:01:44 -0700

    added time UI

---
+++ icedcoffee/Assets/Scripts/Timer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    public Text TimeText;
+
+    void Update () {
+        TimeText.text = DateTime.Now.ToString("HH:mm tt");
+    }
+}
