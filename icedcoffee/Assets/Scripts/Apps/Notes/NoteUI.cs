9908a1a69abc0184f3a459787548f0c2186ae8d6
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2020-02-18 20:30:03 -0800

    notes with no photo no longer have an empty photo icon

--- icedcoffee/Assets/Scripts/Apps/Notes/NoteUI.cs
+++ icedcoffee/Assets/Scripts/Apps/Notes/NoteUI.cs
@@ -1,25 +1,33 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class NoteUI : MonoBehaviour
 {
     public NotesApp NotesApp;
     public Text Text;
-    public Image Image;
+    public PhotoTile PhotoTile;
     public Button Button;
 
     public void SetContent (NotesApp app, string text, Sprite sprite = null) {
+        if(PhotoTile == null || PhotoTile.Image == null) {
+            Assert.IsTrue(PhotoTile != null && PhotoTile.Image != null);
+            return;
+        }
+
         Text.text = text;
         if(sprite != null) {
-            Image.sprite = null;
-            Image.preserveAspect = false;
-            Image.sprite = sprite;
-            Image.preserveAspect = true;
-            Image.gameObject.SetActive(true);
+            PhotoTile.Image.sprite = null;
+            PhotoTile.Image.preserveAspect = false;
+            PhotoTile.Image.sprite = sprite;
+            PhotoTile.Image.preserveAspect = true;
+            PhotoTile.gameObject.SetActive(true);
 
             Button.onClick.AddListener (
                 delegate {app.OpenImageClue(sprite);}
             );
+        } else {
+            PhotoTile.gameObject.SetActive(false);
         }
     }
 }
